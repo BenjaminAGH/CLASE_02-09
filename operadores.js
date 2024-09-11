73dd42d32415de9b7fb92665c10e552a56689034
Alucard Fahrenheit <aluc4rd@kali>
2024-09-11 13:18:06 -0300

    Ejercicios Resueltos

---
+++ operadores.js
@@ -0,0 +1,9 @@
+
+const numero1 = 20;
+const numero2 = 40;
+
+console.log("LA SUMA ES: " + numero1 + numero2);
+console.log("LA RESTA ES: " + (numero1 - numero2));
+console.log("LA MULTIPLIACIÓN ES: " + numero1 * numero2);
+console.log("LA DIVISION ES: " + numero1 / numero2);
+console.log("EL MODULO ES: " + numero1 % numero2);
---
+++ variables-tipos.js
@@ -0,0 +1,10 @@
+
+const nombre = "Benjamin";
+const edad = 21;
+const puedoConducir= true;
+
+console.log(nombre + " - tipo de dato: " + typeof nombre);
+console.log(edad + " - tipo de dato: " + typeof edad);
+console.log(puedoConducir + " - tipo de dato: " + typeof puedoConducir);
+
+
